2304f2fa6f57fe83cf996e71ceccbc5c020c253f
JacobBraun2 at codea-scm <JacobBraun2@codea-scm.aws.mapote.com>
2015-11-13 11:18:26 -0500

    Initial commit

---
+++ tabs/Main.lua
@@ -0,0 +1,30 @@
+-- Hello World
+
+-- Created by: Jacob Braun
+-- Created on: Oct 22nd 2015
+-- Created for: ICS2OR-1
+-- This program says Hello, World!
+-- Let's get this thing started, shall we?
+
+-- Use this function to perform your initial setup
+function setup()
+    print("Hello, World!")
+end
+
+-- This function gets called once every frame
+function draw()
+    -- This sets a dark background color 
+    background(0, 0, 0, 255)
+
+    fill(112, 255, 0, 255)
+  fontSize(48)
+    font("CourierNewPSMT")
+    
+    text("Welcome to the matrix." , 500, 400)
+    
+    
+    
+    -- Do your drawing here
+    
+end
+
